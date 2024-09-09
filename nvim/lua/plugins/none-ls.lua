0aec5250d7c20dc2ff14389ec6e7edc0f70c045b
Kyle Scudder <kyle@kylescudder.co.uk>
2024-09-09 06:46:02 +0000

    feat(lua): added stylua config files

--- nvim/lua/plugins/none-ls.lua
+++ nvim/lua/plugins/none-ls.lua
@@ -1,18 +1,20 @@
 return {
-	"nvimtools/none-ls.nvim",
-	dependencies = {
-		"nvimtools/none-ls-extras.nvim",
-	},
-	config = function()
-		local null_ls = require("null-ls")
-		null_ls.setup({
-			sources = {
-				require("none-ls.diagnostics.eslint_d"),
-				null_ls.builtins.formatting.stylua,
-				null_ls.builtins.formatting.prettier,
-			},
-		})
+  "nvimtools/none-ls.nvim",
+  dependencies = {
+    "nvimtools/none-ls-extras.nvim",
+  },
+  config = function()
+    local null_ls = require("null-ls")
+    null_ls.setup({
+      sources = {
+        require("none-ls.diagnostics.eslint_d"),
+        null_ls.builtins.formatting.stylua.with({
+          MiniExtra = { "--config-path", vim.fn.stdpath("config") .. "/stylua.toml" },
+        }),
+        null_ls.builtins.formatting.prettier,
+      },
+    })
 
-		vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
-	end,
+    vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+  end,
 }
