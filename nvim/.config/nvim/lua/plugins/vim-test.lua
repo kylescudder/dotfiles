5aa0693a75177b8dd4bb92b9c45ee16bec0955b9
Kyle Scudder <kyle@kylescudder.co.uk>
2024-09-16 19:02:29 +0100

    fix(nvim): fixed vim-test keymaps

--- nvim/.config/nvim/lua/plugins/vim-test.lua
+++ nvim/.config/nvim/lua/plugins/vim-test.lua
@@ -3,10 +3,10 @@ return {
   dependencies = {
     "preservim/vimux"
   },
-  vim.keymap.set('leader>t', ':TestNearest<CR>'),
-  vim.keymap.set('leader>T', ':TestFile<CR>'),
-  vim.keymap.set('leader>a', ':TestSuite<CR>'),
-  vim.keymap.set('leader>l', ':TestLast<CR>'),
-  vim.keymap.set('leader>g', ':TestVisit<CR>'),
+  vim.keymap.set('n', 'leader>t', ':TestNearest<CR>'),
+  vim.keymap.set('n', 'leader>T', ':TestFile<CR>'),
+  vim.keymap.set('n', 'leader>a', ':TestSuite<CR>'),
+  vim.keymap.set('n', 'leader>l', ':TestLast<CR>'),
+  vim.keymap.set('n', 'leader>g', ':TestVisit<CR>'),
   vim.cmd("let test#strategy = 'vimux'")
 }
