b7705f4b498b85cdb84b26022a6770e1a4bf7f71
Kyle Scudder <kyle@kylescudder.co.uk>
2024-09-21 15:07:20 +0000

    feat(shmux): readded shmux after subrepo fun

---
+++ shmux/.config/shmux/example.sh
@@ -0,0 +1,21 @@
+SHMUX_DIR="$(dirname "$(readlink -f "$0")")"
+. "$SHMUX_DIR/../util/functions.sh"
+
+# Set up your project with a root dir, and name
+project_root ~/Projects/my_project
+session_name "my_project"
+
+# first, create your session
+new_session
+
+# then, layout your session
+rename_window "code"
+run_command "nvim"
+new_window "servers"
+split_horizontal 50%
+
+# at the end, select the window you want first
+select_window "code"
+
+# then, attach to your session!
+attach_to_session
