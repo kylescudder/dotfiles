38883ac80d71688af24b65b9367e824162a872f2
Kyle Scudder <kyle@kylescudder.co.uk>
2024-12-08 16:03:21 +0000

    feat(shmux): added template for kylescudder.co.uk

---
+++ shmux/.config/shmux/layouts/kylescudder.sh
@@ -0,0 +1,31 @@
+SHMUX_DIR="$(dirname "$(readlink -f "$0")")"
+. "$SHMUX_DIR/../util/functions.sh"
+
+# Set up your project with a root dir, and name
+project_root ~/Documents/Repos/kylescudder.co.uk
+session_name "kylescudder"
+
+# first, create your session
+new_session
+
+# then, layout your session
+rename_window "code"
+run_command "nvim"
+new_window "servers"
+
+rename_pane "bun dev"
+run_command "bun dev"
+split_horizontal 66%
+
+rename_pane "general"
+split_horizontal 50%
+
+rename_pane "prettier"
+run_command "bun format"
+
+
+# at the end, select the window you want first
+select_window "1"
+
+# then, attach to your session!
+attach_to_session
