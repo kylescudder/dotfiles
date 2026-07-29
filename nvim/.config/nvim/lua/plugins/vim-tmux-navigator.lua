bc5910b52ab46160b0bf63435d2e5b7633844a1d
Kyle Scudder <kyle@kylescudder.co.uk>
2026-07-29 07:52:24 +0100

    feat(nvim): added herdr compatible tmux-navigator keybinds

--- nvim/.config/nvim/lua/plugins/vim-tmux-navigator.lua
+++ nvim/.config/nvim/lua/plugins/vim-tmux-navigator.lua
@@ -1,7 +1,22 @@
 return {
   "christoomey/vim-tmux-navigator",
-  vim.keymap.set('n', 'C-h', ':TmuxNavigateLeft<CR>'),
-  vim.keymap.set('n', 'C-j', ':TmuxNavigateDown<CR>'),
-  vim.keymap.set('n', 'C-k', ':TmuxNavigateUp<CR>'),
-  vim.keymap.set('n', 'C-l', ':TmuxNavigateRight<CR>'),
+  config = function()
+    local function nav(win, dir)
+      return function()
+        local prev = vim.api.nvim_get_current_win()
+        vim.cmd.wincmd(win)
+        if vim.api.nvim_get_current_win() == prev then
+          vim.system({ "herdr", "pane", "focus", "--direction", dir, "--current" })
+        end
+      end
+    end
+    for k, m in pairs({
+      ["<C-h>"] = { "h", "left" },
+      ["<C-j>"] = { "j", "down" },
+      ["<C-k>"] = { "k", "up" },
+      ["<C-l>"] = { "l", "right" },
+    }) do
+      vim.keymap.set("n", k, nav(m[1], m[2]), { desc = "herdr nav " .. m[2] })
+    end
+  end,
 }
--- nvim/.config/nvim/lua/vim-options.lua
+++ nvim/.config/nvim/lua/vim-options.lua
@@ -5,9 +5,9 @@ vim.cmd("set shiftwidth=2")
 vim.cmd("set clipboard=unnamedplus")
 vim.g.mapleader = " "
 
-vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
-vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
-vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
-vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
+vim.keymap.set("n", "<c-k>", ":wincmd k<CR>")
+vim.keymap.set("n", "<c-j>", ":wincmd j<CR>")
+vim.keymap.set("n", "<c-h>", ":wincmd h<CR>")
+vim.keymap.set("n", "<c-l>", ":wincmd l<CR>")
 
 vim.wo.number = true
