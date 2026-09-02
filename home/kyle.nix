931e42e4a1ec2abdbcfc1d50f01bb22caebc5695
Kyle Scudder <74150974+kylescudder@users.noreply.github.com>
2026-09-02 19:41:09 +0100

    Import Home Manager audio module

--- home/kyle.nix
+++ home/kyle.nix
@@ -61,6 +61,7 @@ in
   imports = [
     ./modules/ssh.nix
     ./modules/git.nix
+    ./modules/audio.nix
   ];
 
   home.username = username;
@@ -163,40 +164,6 @@ in
     # Helium is not in the stable nixpkgs set used here, so consume its flake.
     inputs.helium.packages.${pkgs.stdenv.hostPlatform.system}.default
   ];
-
-  systemd.user.services.audio-setup = {
-    Unit = {
-      Description = "Configure workstation audio outputs";
-      After = [
-        "pipewire.service"
-        "wireplumber.service"
-      ];
-    };
-
-    Service = {
-      Type = "oneshot";
-      ExecStart = pkgs.writeShellScript "audio-setup" ''
-        ${pkgs.pulseaudio}/bin/pactl set-card-profile \
-          alsa_card.pci-0000_00_1f.3 pro-audio
-
-        ${pkgs.alsa-utils}/bin/amixer -c 2 set IEC958 on
-
-        for _ in $(seq 1 20); do
-          if ${pkgs.pulseaudio}/bin/pactl list short sinks \
-            | grep -q 'alsa_output.pci-0000_00_1f.3.pro-output-1'; then
-            break
-          fi
-          sleep 0.1
-        done
-
-        ${pkgs.pulseaudio}/bin/pactl set-sink-volume \
-          alsa_output.pci-0000_00_1f.3.pro-output-1 30%
-      '';
-      RemainAfterExit = true;
-    };
-
-    Install.WantedBy = [ "default.target" ];
-  };
 
   # Bide is intentionally not packaged yet.
 
