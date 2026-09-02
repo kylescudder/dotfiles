2f118d47c63a03c1f1df4da18578ec4f12bfdebd
Kyle Scudder <74150974+kylescudder@users.noreply.github.com>
2026-09-02 19:47:23 +0100

    Extract Home Manager dotfile links

---
+++ home/modules/dotfiles.nix
@@ -0,0 +1,61 @@
+{ config, lib, inputs, ... }:
+let
+  liveDotfilesRoot =
+    "${config.home.homeDirectory}/Documents/Repos/dotfiles";
+
+  linkPackage = packageName: destinationRoot:
+    let
+      storeRoot = inputs.self + "/${packageName}";
+      files = lib.filesystem.listFilesRecursive storeRoot;
+      storeRootString = toString storeRoot + "/";
+    in
+    builtins.listToAttrs (
+      map
+        (path:
+          let
+            relativePath = builtins.unsafeDiscardStringContext (
+              lib.removePrefix storeRootString (toString path)
+            );
+          in
+          {
+            name =
+              if destinationRoot == ""
+              then relativePath
+              else "${destinationRoot}/${relativePath}";
+
+            value.source =
+              config.lib.file.mkOutOfStoreSymlink
+                "${liveDotfilesRoot}/${packageName}/${relativePath}";
+          })
+        files
+    );
+
+  dotfiles =
+    lib.foldl' lib.recursiveUpdate { } [
+      (linkPackage "hyprland" ".config/hypr")
+      (linkPackage "waybar" ".config/waybar")
+      (linkPackage "nvim" ".config/nvim")
+      (linkPackage "spotifyd" ".config/spotifyd")
+      (linkPackage "fastfetch" ".config/fastfetch")
+      (linkPackage "rofi" ".config/rofi")
+      (linkPackage "yazi" ".config/yazi")
+      (linkPackage "ghostty" ".config/ghostty")
+      (linkPackage "btop" ".config/btop")
+      (linkPackage "wlogout" ".config/wlogout")
+
+      (linkPackage "starship" ".config")
+
+      (linkPackage "bashrc" "")
+      (linkPackage "zsh" "")
+
+      (linkPackage "scripts" ".local/bin")
+      (linkPackage "applications" ".local/share/applications")
+      (linkPackage "icons" ".local/share/icons")
+      (linkPackage "wallpapers" ".local/share/wallpapers")
+    ];
+in
+{
+  # Keep existing dotfiles as source-of-truth during the migration. Individual
+  # configs can be converted to native Home Manager modules independently.
+  home.file = dotfiles;
+}
